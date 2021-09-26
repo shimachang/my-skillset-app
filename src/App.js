db6fd8bce24781cc9583fc407e7310c5d2d4471b
shimachang <shimachang0318@yahoo.co.jp>
2021-09-26 11:16:58 +0900

    class-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { List } from "./List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { description: "クリック前" };
+    }
+    changeDescription() {
+        this.setState({
+            description: "クリック後",
+        });
+    }
+    render() {
+        const { description } = this.state;
+        return (
+            <dib>
+                {description}
+                <List title="リストです" />
+                <button onClick={() => this.changeDescription()}>ボタン</button>
+            </dib>
+        );
+    }
 }
 
 export default App;
---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class List extends React.Component {
+    render() {
+        const {title} = this.props
+        return (
+            <div>
+                <h4>{title}</h4>
+            </div>
+        );
+    }
+}
