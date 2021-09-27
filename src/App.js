735643c8b5aec8eb67ca424b7659b5c01b64abe1
shimachang <shimachang0318@yahoo.co.jp>
2021-09-27 09:52:49 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import { useEffect, useState } from "react";
 import { List } from "./List";
 import { Form } from "./Form";
 import { getLanguages } from "./const/languages";
+import styled from "styled-components";
 
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    padding: 24px 64px 0;
+    border-bottom: 1px solid #e0e0e0;
+`;
+
+const HeaderUl = styled.ul`
+    display: flex;
+    margin: 0;
+    padding: 0;
+`;
+
+const HeaderLi = styled.li`
+    list-style: none;
+    padding: 4px 12px;
+    cursor: pointer;
+    border-bottom: ${(props) => (props.focused ? "2px solid #F44336" : "none")};
+`;
 function App() {
     const [tab, setTab] = useState("list");
     const [langs, setLangs] = useState([]);
@@ -23,13 +43,16 @@ function App() {
     };
     return (
         <div>
-            <header>
-                <ul>
-                    <li onClick={() => setTab("list")}>リスト</li>
-                    <li onClick={() => setTab("form")}>フォーム</li>
-                </ul>
-            </header>
-            <hr />
+            <Header>
+                <HeaderUl>
+                    <HeaderLi focused={tab === "list"} onClick={() => setTab("list")}>
+                        リスト
+                    </HeaderLi>
+                    <HeaderLi focused={tab === "form"} onClick={() => setTab("form")}>
+                        フォーム
+                    </HeaderLi>
+                </HeaderUl>
+            </Header>
             {tab === "list" ? <List langs={langs} /> : <Form onAddLang={addLang} />}
         </div>
     );
