c4160eb70e5a7c460bdf97f10d5a5dbde1756f0e
shimachang <shimachang0318@yahoo.co.jp>
2021-09-29 10:29:49 +0900

    useContext

--- src/FormModal.js
+++ src/FormModal.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 import { Modal } from "./components/modal";
 import { Button } from "./components/botton";
+import { useContext } from "react";
+import { ThemeContext, THEMES } from "./contexts/ThemeContext";
 const Container = styled.div`
     width: 240px;
     border-radius: 10px;
     padding: 24px 36px;
-    background-color: white;
+    color: ${({theme}) => theme.color};
+    background-color: ${({theme}) => theme.backgroundColor};
+    border: ${({theme}) => theme === THEMES.dark ? '2px solid white' : 'none'}
 `;
 
 const ButtonWrapper = styled.div`
@@ -15,9 +19,10 @@ const ButtonWrapper = styled.div`
 `;
 
 export const FormModal = ({ confirm, cancel }) => {
+    const [theme] = useContext(ThemeContext)
     return (
         <Modal>
-            <Container>
+            <Container theme={theme}>
                 <div>本当に作成しますか？</div>
                 <ButtonWrapper>
                     <Button onClick={cancel}>Cancel</Button>
